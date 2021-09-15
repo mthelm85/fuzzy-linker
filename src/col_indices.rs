ba6bef1de69c2608b4862bdddfcbbf6bc9d4d11b
Matt Helm <mthelm85@gmail.com>
2021-09-15 15:29:09 -0500

    fixed missing G in column letters

--- src/col_indices.rs
+++ src/col_indices.rs
@@ -2,7 +2,7 @@ use super::errors::*;
 
 const COLUMN_LETTERS: [char; 26] = [
     'A', 'B', 'C', 'D', 'E', 
-    'F', 'F', 'H', 'I', 'J', 
+    'F', 'G', 'H', 'I', 'J', 
     'K', 'L', 'M', 'N', 'O',
     'P', 'Q', 'R', 'S', 'T', 
     'U', 'V', 'W', 'X', 'Y', 
